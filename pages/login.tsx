14ceb96650f179c94b426bcefa0207cc52fd91f8
何东升 <he77505357@163.com>
2022-05-17 18:25:50 +0800

    feat(app): login page && sass

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,12 +1,19 @@
 import {NextPage} from "next";
 import * as React from "react";
-
+import styles from '../styles/Login.module.scss';
+import PageHead from '../components/PageHead';
 
 
 const Login: NextPage = () => {
-    return <div>
-        login
-        <button className="ui-button">默认按钮</button>
-    </div>
+    return <div className={styles.loginPage}>
+        <PageHead/>
+        <main className={styles.mainContent}>
+            <div>
+                <div>login form</div>
+                <button className="ui-button">默认按钮</button>
+            </div>
+            <div>img</div>
+        </main>
+    </div>;
 };
-export default Login
+export default Login;
---
+++ pages/registry.tsx
@@ -0,0 +1,10 @@
+import {NextPage} from "next";
+import * as React from "react";
+
+const Registry: NextPage = () => {
+    return <div>
+        Registry
+        <button className="ui-button">默认按钮</button>
+    </div>
+};
+export default Registry
